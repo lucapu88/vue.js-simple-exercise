4821bedc22d7997e5b98858dc00e95fd242680b1
lucapu88 <caputoluca88@gmail.com>
2020-04-01 16:10:12 +0200

    splice

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ var app = new Vue({
   methods: {
     addTodo() { //funzione applicata al click del button
       this.todos.push(this.newTodo); //dentro l'array (todos) va ad aggiungere quel singolo elemento che noi scriviamo (newTodo)
+      this.newTodo = ''; //resetto l'input
+    },
+    removeTodo(index, todo) { //funzione applicata al click sull'icona della spazzatura
+      this.todos.splice(index, 1); //elimina l'elemento cliccato perchè gli abbiamo dato index 1 (splice elmina elementi dall'index in poi, index compreso, quindi in questo caso elimina se stesso)
     }
   }
 });
