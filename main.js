e6bea9e2811691c78c3ac8a976e9112e34bff95e
lucapu88 <caputoluca88@gmail.com>
2020-04-02 18:04:01 +0200

    stile

--- main.js
+++ main.js
@@ -25,12 +25,12 @@ const app = new Vue({
       this.saveTodos(); //salvo il tutto
     },
     removeTodo(x) {
-      this.todos.splice(x, 1); //elimina l'elemento cliccato perchè gli abbiamo dato index 1 (splice elmina elementi dall'index in poi, index compreso, quindi in questo caso elimina se stesso)
+      this.todos.splice(x, 1); //elimina l'elemento cliccato perchè gli abbiamo dato come index 1 (splice elmina elementi dall'index in poi, index compreso, quindi in questo caso elimina se stesso)
       this.saveTodos(); //salvo il tutto
     },
-    saveTodos() {
-      const parsed = JSON.stringify(this.todos);
-      localStorage.setItem('todos', parsed);
+    saveTodos() { //per salvare elementi
+      const parsed = JSON.stringify(this.todos); //in una costante racchiudo il todo trasformato in stringa per poter inviare i dati
+      localStorage.setItem('todos', parsed); //imposto il valore in locale passandogli l'array e l'array trasformato in stringa
     },
     removeAllTodo(x) {
       this.todos.splice(x); //elimina tutta la lista
