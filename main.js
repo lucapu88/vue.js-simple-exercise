e67be99433221728f0f7c62873a610064a604d95
lucapu88 <caputoluca88@gmail.com>
2020-04-02 15:48:32 +0200

    parte finale

--- main.js
+++ main.js
@@ -1,27 +1,9 @@
-//
-// var app = new Vue({
-//   el: "#app",
-//   data: {
-//     newTodo:'', //elemento che scriviamo noi e andrà a riempire l'array
-//     todos: [] //array vuoto che conterrà gli elementi che noi digitiamo
-//   },
-//   methods: {
-//     addTodo() { //funzione applicata al click del button
-//       // var thisTodo = localStorage.getItem(this.newTodo);
-//       this.todos.push(this.newTodo); //dentro l'array (todos) va ad aggiungere quel singolo elemento che noi scriviamo (newTodo)
-//       this.newTodo = ''; //resetto l'input
-//       localStorage.setItem(this.todos, JSON.stringify(todos));
-//     },
-//     removeTodo(index, todo) { //funzione applicata al click sull'icona della spazzatura
-//        this.todos.splice(index, 1); //elimina l'elemento cliccato perchè gli abbiamo dato index 1 (splice elmina elementi dall'index in poi, index compreso, quindi in questo caso elimina se stesso)
-//     }
-//   }
-// });
+
 const app = new Vue({
   el: '#app',
   data: {
-    todos: [],
-    newTodo: null
+    todos: [], //array vuoto che conterrà gli elementi che noi digitiamo
+    newTodo: null //elemento che scriviamo noi e andrà a riempire l'array
   },
   mounted() {
     if (localStorage.getItem('todos')) {
@@ -33,19 +15,18 @@ const app = new Vue({
     }
   },
   methods: {
-    addTodo() {
-      // ensure they actually typed something
+    addTodo() { //funzione applicata al click del button
       if (!this.newTodo) {
         return;
       }
 
-      this.todos.push(this.newTodo);
-      this.newTodo = '';
-      this.saveTodos();
+      this.todos.push(this.newTodo); //dentro l'array (todos) va ad aggiungere quel singolo elemento che noi scriviamo (newTodo)
+      this.newTodo = ''; //resetto l'input
+      this.saveTodos(); //salvo il tutto
     },
     removeTodo(x) {
-      this.todos.splice(x, 1);
-      this.saveTodos();
+      this.todos.splice(x, 1); //elimina l'elemento cliccato perchè gli abbiamo dato index 1 (splice elmina elementi dall'index in poi, index compreso, quindi in questo caso elimina se stesso)
+      this.saveTodos(); //salvo il tutto
     },
     saveTodos() {
       const parsed = JSON.stringify(this.todos);
