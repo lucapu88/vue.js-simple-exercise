5464422aae3198e0d3cce0d93ae27e5b136c3318
Luca Caputo <luca.caputo@sorint.it>
2020-11-08 19:54:32 +0100

    aggiunta pulsante di copia (codice scritto da cani, da revisionare tutto)

--- main.js
+++ main.js
@@ -5,15 +5,21 @@ const app = new Vue({
     todos: [{
 
     }], //array di oggetti vuoto che conterrà gli elementi che noi digitiamo
+    list:[{
+
+    }], //lista da condividere che conterrà gli stessi elementi che noi digitiamo
     newTodo: null, //elemento che scriviamo noi e andrà a riempire l'array
     visible: true, //serve per la visibilità del contenitore dell'alert
+    copyList: "Copia Lista", //testo del pulsante copia
   },
   mounted() {
-    if (localStorage.getItem('todos')) { //se si deve prendere un oggetto da salvare in locale
+    if (localStorage.getItem('todos'), localStorage.getItem('list')) { //se si deve prendere un oggetto da salvare in locale
       try {
         this.todos = JSON.parse(localStorage.getItem('todos')); //prova a trasformare l'array in un oggetto javascript
+        this.list = JSON.parse(localStorage.getItem('list')); //prova a trasformare l'array in un oggetto javascript
       } catch(e) {
         localStorage.removeItem('todos'); //se viene trovato un errore, rimuovi l'oggetto (o meglio, non salvare niente)
+        localStorage.removeItem('list'); //se viene trovato un errore, rimuovi l'oggetto (o meglio, non salvare niente)
       }
     }
   },
@@ -31,26 +37,40 @@ const app = new Vue({
         return;
       }
       this.todos.push({ name: this.newTodo, isHidden: true, isActive: false }); //dentro l'array (todos) va ad aggiungere quel singolo elemento che noi scriviamo (newTodo) e gli setta come proprietà isHidden true (che sarebbe visibile in realtà), e isActive false (così inizialmente non ha classe active)
+      this.list.push(this.newTodo + '\n'); //nell'array (list) vado a inserire il todo
       this.newTodo = ''; //resetto l'input
+      this.copyList = "Copia Lista", //resetto il testo del pulsante copia
       this.saveTodos(); //salvo il tutto
     },
     removeTodo(x) {
       this.todos.splice(x, 1); //elimina l'elemento cliccato perchè gli abbiamo dato come index 1 (splice elmina elementi dall'index in poi, index compreso, quindi in questo caso elimina se stesso)
+      this.list.splice(x, 1); //faccio la stessa cosa della riga qui sopra, ma per la lista
+      this.copyList = "Copia Lista", //resetto il testo del pulsante copia
       this.saveTodos(); //salvo il tutto
     },
-    modifyTodo(x, y) { //prendo in pasto x=index e y=todo
+    modifyTodo(x, y, todo) { //prendo in pasto x=index e y=todo
       // console.log(x, y);
-      this.todos.splice(x, 1); //elimino il todo preso in pasto
+      this.todos.splice(x, 1); //elimino da todos il todo preso in pasto
       this.todos.splice(x, 0, y); //aggiungo un nuovo todo passandogli lo stesso index
+      this.list.splice(x, 1); //elimino da list il todo preso in pasto
+      this.list.splice(0, 0, todo + '\n'); //aggiungo in list il nuovo todo
+      this.copyList = "Copia Lista", //resetto il testo del pulsante copia
       this.saveTodos(); //salvo il tutto
     },
     saveTodos() { //per salvare elementi
-      const parsed = JSON.stringify(this.todos); //in una costante racchiudo il todo trasformato in stringa per poter inviare i dati
+      const parsed = JSON.stringify(this.todos, this.list); //in una costante racchiudo il todo trasformato in stringa per poter inviare i dati
       localStorage.setItem('todos', parsed); //imposto il valore in locale passandogli l'array e l'array trasformato in stringa
+      localStorage.setItem('list', parsed); //imposto il valore in locale passandogli l'array e l'array trasformato in stringa
     },
     removeAllTodo(x) {
       this.todos.splice(x); //elimina tutta la lista
+      this.list.splice(x); //faccio la stessa cosa della riga qui sopra, ma per la lista
       this.saveTodos(); //salvo il tutto
+      this.copyList = "Copia Lista", //resetto il testo del pulsante copia
     },
+    copy(list) {
+      navigator.clipboard.writeText(list); //copio negli appunti una lista della spesa per poterla condividere
+      this.copyList = "Copiata" //cambio il testo del pulsante copia
+    }
   }
 });
